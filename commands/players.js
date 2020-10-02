c8e33e3806e6d3cb800758ff65ddb0cf1005b2d2
TS-7 <63742049+TS-7@users.noreply.github.com>
2020-10-02 13:25:38 +0530

    Small update.
    Updated players.js such that everytime there is something valid fetched, it logs all player names and discord ID's. Will update it to embed when the API is fixed

--- commands/players.js
+++ commands/players.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
      exports.run = async(client, message, args) => {
     const fetch = require("node-fetch")
 
-    const data = await fetch("https://servers-live.fivem.net/api/servers/single/7blbde")
+    const data = await fetch("http://fivem.pmarp.com:30120/players.json")
     .then(res => res.json())
 
     const getDiscordID = user => user.players.find(id => id.includes("discord")).replace("discord:", "")
