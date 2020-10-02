bd7298514b93784288177ab0bf94cbfc5ac6733c
TS-7 <illuminati.aternos@gmail.com>
2020-10-02 13:23:06 +0530

    Small update.
    Updated players.json to show all players names when command is run and there is something valid that is fetched

--- commands/players.js
+++ commands/players.js
@@ -3,11 +3,13 @@ const Discord = require('discord.js');
      exports.run = async(client, message, args) => {
     const fetch = require("node-fetch")
 
-    const data = await fetch("https://servers-live.fivem.net/api/servers/single/7blbde")
+    const data = await fetch("http://fivem.pmarp.com:30120/players.json")
     .then(res => res.json())
 
     const getDiscordID = user => user.players.find(id => id.includes("discord")).replace("discord:", "")
-    const DataBlock = data.map(user => ` Name: ${user.name}, Discord: ${getDiscordID(user)}`).join("\n")
+   const DataBlock = data.map(user => ` Name: ${user.name}, Discord: ${getDiscordID(user)}`).join("\n")
 
-    console.log(DataBlock)
+    const information = JSON.stringify(DataBlock);
+
+    console.log(information)
      }
