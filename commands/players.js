2c54997057ce14e2317faf203118674acfbed0d0
TS-7 <illuminati.aternos@gmail.com>
2020-10-03 14:30:23 +0530

    Huge update on players.js
    Made it such that if there are too many players in the city, there is no error that is being thrown out that the embed is exceeding 2000 characters. The bot sends 2 embeds if there are 2000+ characters

--- commands/players.js
+++ commands/players.js
@@ -6,10 +6,34 @@ const Discord = require('discord.js');
     const data = await fetch("http://fivem.pmarp.com:30120/players.json")
     .then(res => res.json())
 
-    const getDiscordID = user => user.players.find(id => id.includes("discord")).replace("discord:", "")
-   const DataBlock = data.map(user => ` Name: ${user.name}, Discord: ${getDiscordID(user)}`).join("\n")
+    const getDiscordID = user => user.identifiers.find(id => id.includes("discord")).replace("discord:", "")
 
-    const information = JSON.stringify(DataBlock);
+const DataBlock = data.map(user => ` **Name:** ${user.name}, **Discord:** *${getDiscordID(user)}*`).join("\n")
 
-    console.log(information)
+          if (DataBlock.length > 2000){
+               let first2000 = DataBlock.slice(0, 2000)
+               const information = (first2000);
+          
+
+          const YeSistaasAss = new Discord.MessageEmbed()
+               .setColor('#800080')
+               .setAuthor('Player List :')
+               .setDescription(information)
+
+       message.channel.send(YeSistaasAss)
+          }
+
+          const YeSistaasAss2 = new Discord.MessageEmbed()
+          .setColor('#800080')
+          .setAuthor('Player List :')
+          .setDescription(DataBlock)
+
+  message.channel.send(YeSistaasAss2)
+
+  // LOGS
+       const Log = new Discord.MessageEmbed()
+          .setColor('#0099ff')
+          .setAuthor(`${message.author.username} used the command /players in ${message.channel.name}`)
+
+          client.channels.cache.get('760543876929945610').send(Log)
      }
