187a8585a8f10b61ed98381e3d20f95f76c56627
TS-7 <illuminati.aternos@gmail.com>
2020-10-03 20:46:20 +0530

    Patch notes for /kick update
    - Added a new ( reason ) field so moderators can give a reason for the kick
    - Improvised the logs to show the reason
    - Added support for even the Admin, Community Manager and Director roles
    - Made a few changes on the reply
    
    - Cleaned the command
    
    --- End of commit. This command is no more going to be changed ---

--- commands/kick.js
+++ commands/kick.js
@@ -1,31 +1,29 @@
 const Discord = require('discord.js')
 
-exports.run = (client, message, [mention, ...reason]) => {
+exports.run = (client, message, args) => {
 
     const channel = client.channels.cache.get('760543876929945610');
-    const modRole = message.guild.roles.cache.find(role => role.name === "Moderator");
     const superMods = ['460078206326800434'];
-
-    if (!modRole)
-      return console.log("You need to have the 'Moderator' role in your server!");
   
-    if (!message.member.roles.cache.has(modRole.id) && !superMods.includes(message.member.id))
+    if (!message.member.roles.cache.some(r =>["Admin", "Moderator", "Community Manager", "Server Director"].includes(r.name)) && !superMods.includes(message.member.id) )
       return message.reply("You are not authorised to use this command you poo poo head");
-  
+
     if (message.mentions.members.size === 0)
-      return message.reply("Who am I supposed to kick? Your ass ?");
+      return message.reply("What am I supposed to kick? Your ass ?");
   
     if (!message.guild.me.hasPermission("KICK_MEMBERS"))
-      return message.reply("I do not have permissions to kick someone:frowning:");
+      return message.reply("I do not have permissions to kick anyone:frowning:");
   
     const kickMember = message.mentions.members.first();
+    let reason = args.slice(1).join(" ");
   
-    kickMember.kick(reason.join(" ")).then(member => {
-      message.reply(`${member.user.username} got his ass kicked!`);
+    kickMember.kick(reason).then(member => {
+      message.reply(`${member.user.username} got his ass kicked. ***Reason*** : *${reason}*`);
 
       const Log = new Discord.MessageEmbed()
-        .setColor('#0099ff')
+        .setColor('#ff0000')
         .setAuthor(`${member.user.username} was kicked from the server by ${message.author.username}.`)
+        .setDescription(`**Reason :** ${reason}`)
 
         channel.send(Log)
     });
